308f4d8b801450f400377527668517accec098f6
The Sharps <Sharp@Thes-MacBook-Air.local>
2018-01-28 20:28:19 -0500

    added middleware

--- workoutLogServer/app.js
+++ workoutLogServer/app.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(require('./middleware/headers'));
+
+app.listen(3000, function(){
+    console.log("app is listening on 3000");
+    });
 
 app.use('/api/test', function(req, res){
 	res.send("Hello World");
 });
 
 
-app.listen(3000, function(){
-    console.log("app is listening on 3000");
-    });
+
     
---
+++ workoutLogServer/middleware/headers.js
@@ -0,0 +1,6 @@
+module.exports = function(req, res, next){
+	res.header('access-control-allow-origin', '*');
+next();
+};
+
+
